76574ac1101ba25d19d5d9797b08254e2a314085
Fabiana Cecin <fabiana@waku.org>
2026-07-29 07:58:43 -0300

    vendor/ces portability: Boost>=1.90 components, Linux-only stacktrace, Ninja byproducts

--- vendor/ces/src/ceslib/util/ctrlc.cpp
+++ vendor/ces/src/ceslib/util/ctrlc.cpp
@@ -2,7 +2,9 @@
  * ctrlc.cpp
  */
 
+#if defined(CES_HAVE_STACKTRACE)
 #include <boost/stacktrace.hpp>
+#endif
 #include <ces/util/ctrlc.h>
 #include <cstdlib>
 #include <iostream>
@@ -14,15 +16,21 @@ volatile std::sig_atomic_t g_interrupted = 0;
 
 extern "C" void interrupt_handler(int) { internal::g_interrupted += 1; }
 
+// The stack trace rides libbacktrace (Linux only); elsewhere the handler
+// still reports and exits, without a trace.
 extern "C" void abrt_handler(int s) {
   std::cerr << "SIGABRT\n";
+#if defined(CES_HAVE_STACKTRACE)
   std::cerr << boost::stacktrace::stacktrace() << std::endl;
+#endif
   std::_Exit(s);
 }
 
 extern "C" void segv_handler(int s) {
   std::cerr << "SIGSEGV\n";
+#if defined(CES_HAVE_STACKTRACE)
   std::cerr << boost::stacktrace::stacktrace() << std::endl;
+#endif
   std::_Exit(s);
 }
 
